a723812c22e126a69bfb7fe4d780305682fd1eac
oh111-beep <ifeanyihezekiah01@email.com>
2026-01-23 11:58:01 +0100

    frist commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Homepage () {
+  return(
+    <main>
+      <div>
+        <p className="text-4xl text-blue-500">welcome to my home page</p>
+      </div>
+    </main>
+  )
+}
